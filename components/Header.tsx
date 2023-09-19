f74ad6b4a6c7247f346270981a5979ab6eebe45b
danny-mak1993 <danny.mak2@gmail.com>
2023-09-19 14:12:43 -0500

    added button component

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,8 @@ import { useRouter } from "next/navigation";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 
+import Button from "./Button";
+
 interface HeaderProps {
   children: React.ReactNode;
   className?: string;

718480221333f24fe4b1246861044dd29435b7a8
danny-mak1993 <danny.mak2@gmail.com>
2023-09-19 14:16:09 -0500

    added button component elements

--- components/Header.tsx
+++ components/Header.tsx
@@ -100,45 +100,15 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
           </button>
         </div>
         <div className="flex justify-between items-center gap-x-4">
-          {user ? (
-            <div className="flex gap-x-4 items-center">
-              <Button onClick={handleLogout} className="bg-white px-6 py-2">
-                Logout
+          <>
+            <div>
+              <Button>
+                Sign up
               </Button>
-              <Button
-                onClick={() => router.push("/account")}
-                className="bg-white"
-              >
-                <FaUserAlt />
-              </Button>
-            </div>
-          ) : (
-            <>
-              <div>
-                <Button
-                  onClick={authModal.onOpen}
-                  className="
-                    bg-transparent 
-                    text-neutral-300 
-                    font-medium
-                  "
-                >
-                  Sign up
-                </Button>
               </div>
-              <div>
-                <Button
-                  onClick={authModal.onOpen}
-                  className="bg-white px-6 py-2"
-                >
-                  Log in
-                </Button>
-              </div>
-            </>
-          )}
+          </>
         </div>
       </div>
-      {children}
     </div>
   );
 };

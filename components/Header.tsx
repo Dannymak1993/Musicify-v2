6236873689e080021e7920b1e91a0333006d9d81
danny-mak1993 <danny.mak2@gmail.com>
2023-09-20 15:22:30 -0500

    adjusted some buttons css using tailwind in header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -112,6 +112,8 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
               >
                 Sign up
               </Button>
+            </div>
+            <div>
               <Button onClick={() => {}} className="bg-white px-6 py-2">
                 Log in
               </Button>

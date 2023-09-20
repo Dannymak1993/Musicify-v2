fb8aa9040cb4614ad04b4eeb835cf64e403c74ac
danny-mak1993 <danny.mak2@gmail.com>
2023-09-20 14:55:39 -0500

    added button component elements

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,9 +1,24 @@
-const Button = () => {
-    return (
-        <div>
-            Button
-        </div>
-    );
-}
+import { forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
+    className,
+  children,
+  disabled,
+  type = 'button',
+  ...props
+}, ref) => {
+      return (
+        <button>
+type={type}
+className={twMerge}
+        </button>
+      )
+    });
+
+Button.displayName = "Button";
 
 export default Button;

7a2bb7740f34623e4efa4a0c9f418ea058544b62
danny-mak1993 <danny.mak2@gmail.com>
2023-09-26 14:49:07 -0500

    added uploadmodal component

---
+++ components/UploadModal.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Modal from "./Modal";
+
+const UploadModal = () => {
+  return (
+    <Modal
+    title="Upload modal title"
+    description="Upload modal description"
+    isOpen
+    onChange={() => {}}
+  >
+    Upload Content
+    </Modal>
+  );
+};
+
+export default UploadModal;

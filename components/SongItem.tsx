cfaa414a2787abce1ac683c24b8a8379c50ed00e
danny-mak1993 <danny.mak2@gmail.com>
2023-09-27 18:48:38 -0500

    added play button component

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 
 import { Song } from "@/types";
+import PlayButton from "./PlayButton";
 import useLoadImage from "@/hooks/useLoadImage";
 
 interface SongItemProps {
@@ -51,8 +52,33 @@ const SongItem: React.FC<SongItemProps> = ({ data, onClick
           alt="Image"
         />
       </div>
+      <div className="flex flex-col items-start w-full pt-4 gap-y-1">
+        <p className="font-semibold truncate w-full">
+          {data.title}
+        </p>
+        <p 
+          className="
+            text-neutral-400 
+            text-sm 
+            pb-4 
+            w-full 
+            truncate
+          "
+        >
+          By {data.author}
+        </p>
+      </div>
+      <div 
+        className="
+          absolute 
+          bottom-24 
+          right-5
+        "
+      >
+        <PlayButton />
+      </div>
     </div>
-     );
+ );
 }
 
 export default SongItem;

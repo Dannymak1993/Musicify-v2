1bcc4743f0c5351a37caf157f9686b129f3c9dda
danny-mak1993 <danny.mak2@gmail.com>
2023-09-27 18:36:11 -0500

    added useloadimage hook

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -1,11 +1,18 @@
 "use client";
 
-const SongItem = () => {
-    return (
-        <div>
-            Song Item
-        </div>
-    );
+import { Song } from "@/types";
+
+interface SongItemProps {
+  data: Song;
+  onClick: (id: string) => void;
 }
+
+const SongItem: React.FC<SongItemProps> = ({ data, onClick 
+}) => {
+  return 
+  <div>
+    Song Item
+</div>;
+};
 
 export default SongItem;

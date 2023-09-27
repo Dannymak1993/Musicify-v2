4df1bc140e1be09f99ae53c8ecc323b17e6ae1a3
danny-mak1993 <danny.mak2@gmail.com>
2023-09-27 18:42:57 -0500

    fixed ability to render images under songs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "gcrvjewtznpergtawllm.supabase.co"
+        ]
+    }
+}
 
 module.exports = nextConfig

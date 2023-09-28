e369a18a4edc0244a5ab785da2ec46b5190d7531
danny-mak1993 <danny.mak2@gmail.com>
2023-09-27 19:03:21 -0500

    added mediaitem component

--- components/Library.tsx
+++ components/Library.tsx
@@ -3,23 +3,30 @@
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
 
+import { Song } from "@/types";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
 
-const Library = () => {
-    const authModal = useAuthModal();
-    const uploadModal = useUploadModal();
-    const { user } = useUser();
+import MediaItem from "./MediaItem";
 
-    const onClick = () => {
-      if (!user) {
-        return authModal.onOpen();
-      }
+interface LibraryProps {
+  songs: Song[];
+}
 
-      //check for subscription
+const Library: React.FC<LibraryProps> = ({ songs }) => {
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
 
-      return uploadModal.onOpen();
+  const onClick = () => {
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    //check for subscription
+
+    return uploadModal.onOpen();
   };
 
   return (
@@ -41,7 +48,13 @@ const Library = () => {
         />
       </div>
       <div className="flex flex-col gap-y-2 mt-4 px-3">
-        List of Songs!          
+        {songs.map((item) => (
+          <MediaItem
+            onClick={() => {}}
+            key={item.id}
+            data={item}
+          />
+        ))}
       </div>
     </div>
   );

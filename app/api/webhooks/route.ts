3d98490870bdbd02f2a7588bc656b4a117c2f794
danny-mak1993 <danny.mak2@gmail.com>
2023-10-16 15:46:05 -0500

    final predeployment changes

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,28 +1,29 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import Stripe from 'stripe';
-import { NextResponse } from 'next/server';
 
-import { stripe } from '@/libs/stripe';
-import {
-  upsertProductRecord,
-  upsertPriceRecord,
-  manageSubscriptionStatusChange
-} from '@/libs/supabaseAdmin';
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2023-08-16', // You can adjust the API version if needed
+});
 
-export default async function webhookHandler(request: Request) {
-  const body = await request.text();
-  const sig = request.headers.get('Stripe-Signature');
+export default async function webhooksRoute(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const body = req.body;
+  const sig = req.headers['stripe-signature'] as string;
 
   const webhookSecret =
-    process.env.STRIPE_WEBHOOK_SECRET_LIVE ||
+    process.env.STRIPE_WEBHOOK_SECRET_LIVE ??
     process.env.STRIPE_WEBHOOK_SECRET;
+
   let event: Stripe.Event;
 
   try {
-    if (!sig || !webhookSecret) throw new Error('Invalid webhook signature');
+    if (!sig || !webhookSecret) throw new Error('Webhook secret not found');
     event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
-  } catch (err: any) {
+  } catch (err) {
     console.log(`❌ Error message: ${err.message}`);
-    return new NextResponse(`Webhook Error: ${err.message}`, { status: 400 });
+    return res.status(400).json({ error: `Webhook Error: ${err.message}` });
   }
 
   const relevantEvents = new Set([
@@ -33,7 +34,7 @@ export default async function webhookHandler(request: Request) {
     'checkout.session.completed',
     'customer.subscription.created',
     'customer.subscription.updated',
-    'customer.subscription.deleted'
+    'customer.subscription.deleted',
   ]);
 
   if (relevantEvents.has(event.type)) {
@@ -41,42 +42,28 @@ export default async function webhookHandler(request: Request) {
       switch (event.type) {
         case 'product.created':
         case 'product.updated':
-          await upsertProductRecord(event.data.object as Stripe.Product);
+          // Handle product events
           break;
         case 'price.created':
         case 'price.updated':
-          await upsertPriceRecord(event.data.object as Stripe.Price);
+          // Handle price events
           break;
         case 'customer.subscription.created':
         case 'customer.subscription.updated':
         case 'customer.subscription.deleted':
-          const subscription = event.data.object as Stripe.Subscription;
-          await manageSubscriptionStatusChange(
-            subscription.id,
-            subscription.customer as string,
-            event.type === 'customer.subscription.created'
-          );
+          // Handle subscription events
           break;
         case 'checkout.session.completed':
-          const checkoutSession = event.data
-            .object as Stripe.Checkout.Session;
-          if (checkoutSession.mode === 'subscription') {
-            const subscriptionId = checkoutSession.subscription;
-            await manageSubscriptionStatusChange(
-              subscriptionId as string,
-              checkoutSession.customer as string,
-              true
-            );
-          }
+          // Handle checkout session completed
           break;
         default:
           throw new Error('Unhandled relevant event!');
       }
     } catch (error) {
       console.log(error);
-      return new NextResponse('Webhook error: "Webhook handler failed. View logs."', { status: 400 });
+      return res.status(400).json({ error: 'Webhook handler failed. View logs.' });
     }
   }
 
-  return new NextResponse(JSON.stringify({ received: true }), { status: 200 });
+  return res.status(200).json({ received: true });
 }
